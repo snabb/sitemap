331ab5d481e5b7bb83c662e6cc254bd34ffd5fe1
Janne Snabb <snabb@epipe.com>
2017-12-25 19:11:51 +0200

    Improve SitemapIndex example.

--- sitemapindex_test.go
+++ sitemapindex_test.go
@@ -10,7 +10,7 @@ func ExampleSitemapIndex() {
 	smi := sitemap.NewSitemapIndex()
 	t := time.Unix(0, 0).UTC()
 	smi.Add(&sitemap.URL{
-		Loc:     "http://example.com/",
+		Loc:     "http://example.com/sitemap-1.xml",
 		LastMod: &t,
 	})
 	smi.WriteTo(os.Stdout)
@@ -18,7 +18,7 @@ func ExampleSitemapIndex() {
 	// <?xml version="1.0" encoding="UTF-8"?>
 	// <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	//   <sitemap>
-	//     <loc>http://example.com/</loc>
+	//     <loc>http://example.com/sitemap-1.xml</loc>
 	//     <lastmod>1970-01-01T00:00:00Z</lastmod>
 	//   </sitemap>
 	// </sitemapindex>
