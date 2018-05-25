3ccfa2476d5fe1a47b20433efbe1f6b017ec825a
Guiqiang Zhang <skyhit@gmail.com>
2018-05-25 14:27:43 +0800

    add more optional field

--- sitemap.go
+++ sitemap.go
@@ -29,16 +29,18 @@ const (
 
 // Publication for new
 type Publication struct {
-	Name  string `xml:"name,omitempty"`
-	Language string `xml:"language,omitempty"`
+	Name     string `xml:"name,omitempty"` // Name of the news publication. It must exactly match the name as it appears on your articles in news.google.com, omitting any trailing parentheticals. For example, if the name appears in Google News as "The Example Times (subscription)", you should use "The Example Times". Required.
+	Language string `xml:"language"`  // Language of the publication. It should be an ISO 639 Language Code (either 2 or 3 letters); see: http://www.loc.gov/standards/iso639-2/php/code_list.php Exception: For Chinese, please use zh-cn for Simplified Chinese or zh-tw for Traditional Chinese. Required.
 }
 
 // News entry following the format 
 type News struct {
-	Publication     Publication `xml:"publication,omitempty"`
-	Title           string      `xml:"title,omitempty"`
-	PublicationDate *time.Time  `xml:"publication_date,omitempty"`
-	Genres          string      `xml:"genres,omitempty"`
+	Publication     Publication `xml:"publication"`  // The publication in which the article appears. Required.
+	Title           string      `xml:"title,omitempty"`   // Title of the news article.
+	PublicationDate *time.Time  `xml:"publication_date,omitempty"` // Article publication date in W3C format, specifying the complete date (YYYY-MM-DD) with optional timestamp. See: http://www.w3.org/TR/NOTE-datetime Please ensure that you give the original date and time at which the article was published on your site; do not give the time at which the article was added to your Sitemap. Required.
+	Genres          string      `xml:"genres,omitempty"` // A comma-separated list of properties characterizing the content of the article, such as "PressRelease" or "UserGenerated". For a list of possible values, see: http://www.google.com/support/news_pub/bin/answer.py?answer=93992 Required if any genres apply to the article, otherwise this tag should be omitted.
+	Keywords        string      `xml:"keywords,omitempty"`  // Comma-separated list of keywords describing the topic of the article. Keywords may be drawn from, but are not limited to, the list of existing Google News keywords; see: http://www.google.com/support/news_pub/bin/answer.py?answer=116037 Optional.
+	StockTickers    string      `xml:"stock_tickers,omitempty"`  // Comma-separated list of up to 5 stock tickers of the companies, mutual funds, or other financial entities that are the main subject of the article. Relevant primarily for business articles. Each ticker must be prefixed by the name of its stock exchange, and must match its entry in Google Finance. For example, "NASDAQ:AMAT" (but not "NASD:AMAT"), or "BOM:500325" (but not "BOM:RIL"). Optional.
 }
 
 // URL entry in sitemap or sitemap index. LastMod is a pointer
