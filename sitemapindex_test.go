7195e902d31a0a7b44f9791be7fd483c4d51d03c
Janne Snabb <snabb@epipe.com>
2023-02-22 20:19:50 +0200

    Add more unit tests

--- sitemapindex_test.go
+++ sitemapindex_test.go
@@ -1,9 +1,14 @@
 package sitemap_test
 
 import (
+	"bytes"
+	"fmt"
+	"math/rand"
 	"os"
+	"testing"
 	"time"
 
+	"github.com/go-test/deep"
 	"github.com/snabb/sitemap"
 )
 
@@ -24,4 +29,47 @@ func ExampleSitemapIndex() {
 	//     <lastmod>1970-01-01T00:00:00Z</lastmod>
 	//   </sitemap>
 	// </sitemapindex>
+}
+
+func TestSitemapIndex_WriteToError(t *testing.T) {
+	smi := sitemap.NewSitemapIndex()
+	smi.Add(&sitemap.URL{Loc: "http://example.com/sitemap.xml"})
+
+	n, err := smi.WriteTo(failWriter{})
+	if n != 0 {
+		t.Error("WriteTo did not return zero")
+	}
+	if err == nil {
+		t.Error("WriteTo did not propagate error")
+	}
+}
+
+func TestSitemapIndex_ReadFrom(t *testing.T) {
+	smi1 := sitemap.NewSitemapIndex()
+
+	for i := 0; i < rand.Intn(100)+1; i++ {
+		timeNow := time.Now()
+		smi1.Add(&sitemap.URL{
+			Loc:     fmt.Sprintf("http://example.com/sitemap-%03d.xml", i),
+			LastMod: &timeNow,
+		})
+	}
+
+	buf := new(bytes.Buffer)
+
+	_, err := smi1.WriteTo(buf)
+	if err != nil {
+		t.Fatalf("Error writing sitemap: %v", err)
+	}
+
+	smi2 := new(sitemap.SitemapIndex)
+
+	_, err = smi2.ReadFrom(buf)
+	if err != nil {
+		t.Fatalf("Error reading sitemap: %v", err)
+	}
+
+	if diff := deep.Equal(smi1.URLs, smi2.URLs); diff != nil {
+		t.Error(diff)
+	}
 }
