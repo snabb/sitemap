18da45ef092a88ef5e7d1c4dc4795ee661329e85
Janne Snabb <snabb@epipe.com>
2023-02-22 20:19:50 +0200

    Add more unit tests

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/snabb/sitemap
 go 1.19
 
 require github.com/snabb/diagio v1.0.3
+
+require github.com/go-test/deep v1.1.0
--- sitemap_test.go
+++ sitemap_test.go
@@ -1,12 +1,18 @@
 package sitemap_test
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"testing"
 	"time"
 
+	"github.com/go-test/deep"
 	"github.com/snabb/sitemap"
 )
 
@@ -70,4 +76,58 @@ func ExampleSitemap_minify() {
 	// Output:
 	// <?xml version="1.0" encoding="UTF-8"?>
 	// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>http://example.com/</loc><lastmod>1984-01-01T00:00:00Z</lastmod><changefreq>weekly</changefreq><priority>0.5</priority></url></urlset>
+}
+
+// failWriter is a Writer that always fails.
+type failWriter struct{}
+
+func (failWriter) Write(p []byte) (n int, err error) {
+	return 0, errors.New("write failure")
+}
+
+var _ io.Writer = (*failWriter)(nil)
+
+func TestSitemap_WriteToError(t *testing.T) {
+	sm := sitemap.New()
+	sm.Add(&sitemap.URL{Loc: "http://example.com/"})
+
+	n, err := sm.WriteTo(failWriter{})
+	if n != 0 {
+		t.Error("WriteTo did not return zero")
+	}
+	if err == nil {
+		t.Error("WriteTo did not propagate error")
+	}
+}
+
+func TestSitemap_ReadFrom(t *testing.T) {
+	sm1 := sitemap.New()
+
+	for i := 0; i < rand.Intn(100)+1; i++ {
+		timeNow := time.Now()
+		sm1.Add(&sitemap.URL{
+			Loc:        fmt.Sprintf("http://example.com/%03d.html", i),
+			LastMod:    &timeNow,
+			ChangeFreq: sitemap.Always,
+			Priority:   rand.Float32(),
+		})
+	}
+
+	buf := new(bytes.Buffer)
+
+	_, err := sm1.WriteTo(buf)
+	if err != nil {
+		t.Fatalf("Error writing sitemap: %v", err)
+	}
+
+	sm2 := new(sitemap.Sitemap)
+
+	_, err = sm2.ReadFrom(buf)
+	if err != nil {
+		t.Fatalf("Error reading sitemap: %v", err)
+	}
+
+	if diff := deep.Equal(sm1.URLs, sm2.URLs); diff != nil {
+		t.Error(diff)
+	}
 }
